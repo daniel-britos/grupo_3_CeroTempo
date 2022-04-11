f41d74f2f8e1f7da6526b644413e4a9ceb8ead56
carmenquispechejo <carmenquispechejo@gmail.com>
2022-04-11 19:48:39 -0300

    detalle-maria

--- CeroTempo-Site/app.js
+++ CeroTempo-Site/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/users', usersRouter);
 app.get('/',(req, res) => res.sendFile(path.resolve(__dirname,'views','index.html')));
 
-
+app.get('/productDetail',(req, res) => res.sendFile(path.resolve(__dirname,'views','productDetail.html')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
