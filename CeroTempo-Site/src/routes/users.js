96f89909cfdbf2076f55df42148f0c063c42b5ab
daniel-britos <danielbritospersonal@gmail.com>
2022-04-30 23:15:21 -0300

    ejs y estructura

--- CeroTempo-Site/src/routes/users.js
+++ CeroTempo-Site/src/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+const {register, login} = require('../controllers/usersController');
+
+router.get('/register', register);
+
+router.get('/login', login);
+
 module.exports = router;
