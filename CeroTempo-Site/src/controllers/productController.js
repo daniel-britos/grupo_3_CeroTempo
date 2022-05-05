a1427cac734a2c137cfd36b1d75e95d76db5a412
fernando-eb2406 <feb-332@hotmail.com>
2022-05-05 14:45:00 -0300

    actualiza estructura

---
+++ CeroTempo-Site/src/controllers/productController.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pCart : (req, res) => {
+        res.render('products/productCart');
+    },
+    pDetail : (req, res) => {
+        res.render('products/productDetail');
+    }
+}
