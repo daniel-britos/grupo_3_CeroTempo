ea83f4209987c56863e3d487bc71a86ac2ef81eb
daniel-britos <danielbritospersonal@gmail.com>
2022-05-05 22:52:07 -0300

    form-create

--- CeroTempo-Site/src/routes/admin.js
+++ CeroTempo-Site/src/routes/admin.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 const { create } = require('../controllers/adminController');
 
+
+
 router.get('/create', create);
 
+//http://localhost:3000/create
 
 module.exports = router;
