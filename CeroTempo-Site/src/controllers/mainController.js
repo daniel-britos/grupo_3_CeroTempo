f1ba0f25a2a9a147de6b00da809cef03d36a009c
carmenquispechejo <carmenquispechejo@gmail.com>
2022-05-06 10:46:27 -0300

    actualiza estructura

--- CeroTempo-Site/src/controllers/mainController.js
+++ CeroTempo-Site/src/controllers/mainController.js
@@ -2,6 +2,15 @@ module.exports = {
     index : (req, res) => {
         return res.render('index');
     },
+<<<<<<< HEAD
+    create : (req, res) => {
+        return res.render('admin/create');
+    },
+    edit : (req, res) => {
+        return res.render('admin/edit');
+    },
+=======
+>>>>>>> productCart-fernando
     courses : (req, res) => {
         return res.render('courses');
     },
