4dac4b27dd14d89b323b24f45dd0f5abfcb4c39f
fernando-eb2406 <feb-332@hotmail.com>
2022-05-06 15:06:42 -0300

    actualiza etiquetas

--- CeroTempo-Site/src/app.js
+++ CeroTempo-Site/src/app.js
@@ -3,10 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-<<<<<<< HEAD
-const methodOverride = require('method-override')
-=======
->>>>>>> productCart-fernando
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,11 +11,7 @@ var productsRouter = require('./routes/products');
 var app = express();
 
 // view engine setup
-<<<<<<< HEAD
-app.set('views', path.join(__dirname, 'views')); // lo escribimos asi para usar sub carpetas
-=======
 app.set('views', path.join(__dirname, '/views/')); // lo escribimos asi para usar sub carpetas
->>>>>>> productCart-fernando
 app.set('view engine', 'ejs');
 
 
@@ -28,20 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..' ,'public')));
-<<<<<<< HEAD
-app.use(methodOverride('_method'));
-
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter); 
-
-=======
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter); // http://localhost:3000/products/productCart
->>>>>>> productCart-fernando
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
