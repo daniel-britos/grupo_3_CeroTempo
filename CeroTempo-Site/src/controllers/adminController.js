a434f8b31de0698097d8fc89677cd221d649baa6
carmenquispechejo <carmenquispechejo@gmail.com>
2022-05-19 19:26:17 -0300

    add-create

--- CeroTempo-Site/src/controllers/adminController.js
+++ CeroTempo-Site/src/controllers/adminController.js
@@ -1,3 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+
+const readProduct = () => {  
+	const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); 
+    return products
+}
+const saveProducts = (product) => fs.writeFileSync(productsFilePath, JSON.stringify(product, null, 3))
+
 module.exports = {
     panel : (req, res) => {
         return res.render('admin/panel');
@@ -5,7 +16,24 @@ module.exports = {
     create : (req, res) => {
         return res.render('admin/create');
     },
+    store : (req, res) =>{
+        let products = readProduct()
+        const {id, name, price, discount, category, detail, characteristics, image} = req.body;
+        let oProducts = {
+            id: products[products.length-1].id+1,
+            name: name.trim(),
+            price: +price,
+            discount: +discount,
+            category: category,
+            detail: detail,
+            characteristics: characteristics,
+            image: req.file? req.filename: 'default-image.png'
+        }
+        products.push(oProducts)
+        saveProducts(products)
+        return res.redirect('admin/create');
+    },
     edit : (req, res) => {
         return res.render('admin/edit');
-    },
+    }
 }
--- CeroTempo-Site/src/routes/admin.js
+++ CeroTempo-Site/src/routes/admin.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const  {panel, create, edit} = require('../controllers/adminController');
+const  {panel, create, store, edit} = require('../controllers/adminController');
 
 
 router.get('/panel', panel);
 router.get('/create', create);
+router.post('/create', store);
 router.get('/edit', edit);
 
 module.exports = router;
