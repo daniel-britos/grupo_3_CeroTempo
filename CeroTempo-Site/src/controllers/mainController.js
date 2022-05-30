c62381128755b9f897f5c5e0493689992a33e4eb
belgonzalez21 <nsbelg@gmail.com>
2022-05-30 13:51:22 -0300

    iteration of home

--- CeroTempo-Site/src/controllers/mainController.js
+++ CeroTempo-Site/src/controllers/mainController.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+
+const readProduct = () => {  
+	const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); 
+    return products
+}
 module.exports = {
     index : (req, res) => {
-        return res.render('index');
+        const listProduct = readProduct()
+        res.render('index', {
+            listProduct
+        });
     },
     courses : (req, res) => {
         return res.render('courses');
