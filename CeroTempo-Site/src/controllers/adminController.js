ac9208f69ad5a3143cadfa9ad3b76fd699715018
carmenquispechejo <carmenquispechejo@gmail.com>
2022-05-30 13:00:43 -0300

    multer-maria

--- CeroTempo-Site/src/controllers/adminController.js
+++ CeroTempo-Site/src/controllers/adminController.js
@@ -1,11 +1,63 @@
 module.exports = {
-    panel : (req, res) => {
+    panel: (req, res) => {
         return res.render('admin/panel');
     },
-    create : (req, res) => {
+    create: (req, res) => {
         return res.render('admin/create');
     },
-    edit : (req, res) => {
-        return res.render('admin/edit');
+    store: (req, res) => {
+        let products = readProduct()
+        const { name, price, discount, category, detail, characteristics, image } = req.body;
+        let oProducts = {
+            id: products[products.length - 1].id + 1,
+            name: name.trim(),
+            price: +price,
+            discount: +discount,
+            category: category,
+            detail: detail.trim(),
+            characteristics: characteristics.trim(),
+            default: req.file ? req.file.filename : 'default-image.png' ,
+        }
+        products.push(oProducts);
+        saveProducts(products);
+        return res.redirect('create');
     },
+    edit: (req, res) => {
+        let products = readProduct()
+        let product = products.find(product => product.id === +req.params.id);
+        return res.render('edit', {
+            product
+        });
+    },
+    update: (req, res) => {
+        let products = readProduct();
+        const { name, price, discount, category, detail, characteristics, image } = req.body;
+        const product = products.find(product => product.id === +req.params.id);
+        const updateProducts = products.map(product => {
+            if (product.id === +req.params.id) {
+                let updateProduct = {
+                    ...product,
+                    name: name.trim(),
+                    price: +price,
+                    discount: +discount,
+                    category: category,
+                    detail: detail.trim(),
+                    characteristics: characteristics.trim(),
+                    image: "default-image.png"
+                }
+                return updateProduct;
+            }
+            return product;
+        })
+        saveProducts(updateProducts);
+        return res.redirect('productMain');
+    },
+    remove: (req, res) => {
+        let products = readProduct();
+        //leo lo sproductos
+        const productsModify = products.filter(product => product.id !== +req.params.id);
+        // traigo todos los que son distintos al id que ingresa por parametro
+        saveProducts(productsModify);
+        return res.redirect('productMain');
+    }
 }
--- CeroTempo-Site/src/routes/admin.js
+++ CeroTempo-Site/src/routes/admin.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-const  {panel, create, edit} = require('../controllers/adminController');
+const path = require('path');
+const multer = require('multer');
+
+const storage= multer.diskStorage({destination:(req,file,callback)=>{
+    callback(null,'public/images/instruments')
+}, filename: (req,file, callback)=>{
+    callback(null, file.fieldname+'-'+ Date.now()+ path.extname(file.originalname))
+}
+});
+
+const upload= multer({storage}); 
+
+const  {panel, create, edit, update, remove} = require('../controllers/adminController');
 
 
 router.get('/panel', panel);
 router.get('/create', create);
-router.get('/edit', edit);
+router.get('/create', upload.array('image'), store);
+router.get('/edit/:id', edit);
+router.put('/update/:id', upload.array('image'), update);
+router.delete("/remove/:id", remove);
 
 module.exports = router;
