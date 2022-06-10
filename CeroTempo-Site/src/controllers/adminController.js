fe950f77260c04c207fa891ecb95b7745cbc1f95
belgonzalez21 <nsbelg@gmail.com>
2022-06-09 21:05:40 -0300

    no funciona array

--- CeroTempo-Site/src/controllers/adminController.js
+++ CeroTempo-Site/src/controllers/adminController.js
@@ -1,52 +1,75 @@
+const { validationResult } = require("express-validator");
 const fs = require('fs');
 const path = require('path');
+const products = require("../data/productsDataBase.json");
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-
-const readProduct = () => {  
-	const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); 
-    return products
-}
-const saveProducts = (products) => fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 3))
+// const readProduct = () => {  
+// 	const products = JSON.parse(fs.readFileSync(products, 'utf-8')); 
+//     return products
+// }
+// const saveProducts = (products) => fs.writeFileSync(products, JSON.stringify(products, null, 3))
 
 module.exports = {
     panel: (req, res) => {
         return res.render('panel');
     },
+
+
     create: (req, res) => {
         return res.render('create');
     },
+
+
     store: (req, res) => {
-        let products = readProduct()
-        const { name, price, discount, category, detail, characteristics, image } = req.body;
-        let oProducts = {
-            id: products[products.length - 1].id + 1,
-            name: name.trim(),
-            price: +price,
-            discount: +discount,
-            category: category,
-            detail: detail.trim(),
-            characteristics: characteristics.trim(),
-            image: req.file ? req.file.filename : 'default-image.png' ,
-        }
-        products.push(oProducts);
-        saveProducts(products);
-        return res.redirect('create');
+        let errors = validationResult(req);
+        if(errors.isEmpty()){
+            let { name, price, discount, category, detail, characteristics } = req.body;
+            let lastID = products[products.length - 1].id;
+            let images = req.files.map((img) => img.filename);
+            let newProduct = {
+                id: +lastID + 1,
+                name: name.trim(),
+                price: +price,
+                discount: +discount,
+                category: category,
+                detail: detail.trim(),
+                characteristics: characteristics.trim(),
+                image: images.length > 0 ? images : ['default-image.png']
+            };
+            products.push(newProduct);
+
+            fs.writeFileSync(
+                path.resolve(__dirname, "..", "data", "productsDataBase.json"),
+                JSON.stringify(products, null, 3),
+                "utf-8"
+              );
+              return res.redirect('/');
+        }else{
+        console.log(errors.mapped());
+        return res.render("create", {
+        errors: errors.mapped(),
+        old: req.body,
+      });
+    }        
+        // saveProducts(products);
     },
+
     edit: (req, res) => {
-        let products = readProduct()
-        let product = products.find(product => product.id === +req.params.id);
+        const {id} = req.params;
+        let product = products.find((product) => product.id === +id);
         return res.render('edit', {
             product
         });
     },
     update: (req, res) => {
-        let products = readProduct();
-        const { name, price, discount, category, detail, characteristics, image, } = req.body;
-        const product = products.find(product => product.id === +req.params.id);
-        const updateProducts = products.map(product => {
-            if (product.id === +req.params.id) {
-                let updateProduct = {
+        let errors = validationResult(req);
+        if (errors.isEmpty()){
+            const{id} = req.params;
+            const { name, price, discount, category, detail, characteristics} = req.body;
+        
+        const productsModify = products.map((product) => {
+            if(product.id === +id){
+                let productModify = {
                     ...product,
                     name: name.trim(),
                     price: +price,
@@ -54,21 +77,52 @@ module.exports = {
                     category: category,
                     detail: detail.trim(),
                     characteristics: characteristics.trim(),
-                    image: req.file ? req.file.filename : 'default-image.png',
+                    image: req.file ? req.file.filename : product.image,
+                };
+                if(req.file){
+                    if(
+                        fs.existsSync(
+                            path.resolve(__dirname, "..", "public", "images", "instruments", product.image)
+                          ) &&
+                          product.image !== "default-image.png"
+                    ){
+                        fs.unlinkSync(
+                            path.resolve(__dirname, "..", "public", "images", "instruments", product.image)
+                          ); 
+                    }
                 }
-                return updateProduct;
+                return productModify;
             }
             return product;
-        })
-        saveProducts(updateProducts);
+            
+        });
+        fs.writeFileSync(
+        path.resolve(__dirname, "..", "data", "productsDataBase.json"),
+        JSON.stringify(productsModify, null, 3),
+        "utf-8"
+        );
         return res.redirect('productMain');
-    },
+    }else{
+        console.log(errors);
+        return res.render("create", {
+            product : req.body,
+            errors : errors.mapped()
+        });
+    }
+},
+
     remove: (req, res) => {
-        let products = readProduct();
+        const { id } = req.params;
+        // let products = readProduct();
         //leo lo sproductos
-        const productsModify = products.filter(product => product.id !== +req.params.id);
+        const productFilter= products.filter((product) => product.id !== +id);
         // traigo todos los que son distintos al id que ingresa por parametro
-        saveProducts(productsModify);
+        fs.writeFileSync(
+            path.resolve(__dirname, "..", "data", "productsDataBase.json"),
+            JSON.stringify(productFilter, null, 3),
+            "utf-8"
+          );
+        // saveProducts(productsModify);
         return res.redirect('productMain');
     }
 }
