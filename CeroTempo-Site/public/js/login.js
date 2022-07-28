b20a63cd40b63a2e590730377649557a3b27c8a2
fernando-eb2406 <feb-332@hotmail.com>
2022-07-28 18:13:43 -0300

    validaciones front - login/contraseña

--- CeroTempo-Site/public/js/login.js
+++ CeroTempo-Site/public/js/login.js
@@ -3,6 +3,7 @@ console.log('login.js success!');
 const regExEmail =
   /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
 
+/* Validacion email*/
 $('email').addEventListener('blur', function () {
   switch (true) {
     case !this.value.trim():
@@ -21,30 +22,17 @@ $('email').addEventListener('blur', function () {
   }
 });
 
-/* $('form-login').addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  let elements = e.target.elements;
-  let error = false;
-  
-  for (let i = 0; i < elements.length - 2; i++) {
-    if (!elements[i].value.trim()) {
-      elements[i].classList.add('is-invalid');
-      error = true;
-      $('error-pass').innerHTML = 'Los campos señalados son obligatorios';
-    }
+/* Validacion pass*/
+$('password').addEventListener('blur', function () {
+  switch (true) {
+    case !this.value.trim():
+      this.classList.add('is-invalid');
+      $('errorPassword').innerHTML = 'Debes ingresar tu contraseña';
+      break;
+    default:
+      this.classList.remove('is-invalid');
+      this.classList.add('is-valid');
+      $('errorPassword').innerHTML = null;
+      break;
   }
-
-  for (let i = 0; i < elements.length - 2; i++) {
-    if (elements[i].classList.contains('is-invalid')) {
-      error = true;
-    }
-  }
-
-  !error && e.target.submit();
-}); */
-
-/* No envio el formulario si esta vacio */
-$('form-login').addEventListener('submit', (e) => {
-  e.preventDefault();
 });
