fe2c363aeaa5e39be0aed88b7de918af38ac9241
daniel-britos <danielbritospersonal@gmail.com>
2022-08-03 14:05:41 -0300

    bar

--- CeroTempo-Site/public/js/menubar.js
+++ CeroTempo-Site/public/js/menubar.js
@@ -1,13 +1,6 @@
 const toggleMenuElement = document.getElementById("toggle-menu");
 const mainMenuElement = document.getElementById("menu-bar");
 
-// const toggleProducts = document.getElementById("toggle-products");
-// const menuProducts = document.getElementById("menu-products");
-
 toggleMenuElement.addEventListener("click", () => {
   mainMenuElement.classList.toggle("menu-horizontal--show");
 });
-
-// toggleProducts.addEventListener("click", () => {
-//   menuProducts.classList.toggle("menu-vertical--show");
-// });
