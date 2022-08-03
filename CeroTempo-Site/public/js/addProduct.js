d17d5c58d77f34fe5222d9240978d6278cda8bd4
fernando-eb2406 <feb-332@hotmail.com>
2022-08-03 18:35:24 -0300

    correciones

--- CeroTempo-Site/public/js/addProduct.js
+++ CeroTempo-Site/public/js/addProduct.js
@@ -1,142 +1,142 @@
-let qs = (selector) => document.querySelector(selector); //función para reemplazar todos los querySelector del archivo
-window.addEventListener("load", () => {
+/* let qs = (selector) => document.querySelector(selector); */ //función para reemplazar todos los querySelector del archivo
+
+window.addEventListener('load', () => {
   //carga toda la pantalla.
 
   //creación de todas las variables, para evitar colocar let, uso las comas ","
-  let addProduct = qs("#addProduct"),
+  let addProduct = qs('#addProduct'),
     //***********capturo los inputs por id*****************/
-    name = qs("#name"),
-    price = qs("#price"),
-    discount = qs("#discount"),
-    description = qs("#description"),
-    category = qs("#category"),
-    image = qs("#image"),
+    name = qs('#name'),
+    price = qs('#price'),
+    discount = qs('#discount'),
+    description = qs('#description'),
+    category = qs('#category'),
+    image = qs('#image'),
     regExExtensions = /(.jpg|.jpeg|.png|.gif)$/i, //Formatos válidos.
-    errorName = qs("#errorName"), //****************** capturo los spans por id ************/
-    errorPrice = qs("#errorPrice"),
-    errorDiscount = qs("#errorDiscount"),
-    errorDescription = qs("#errorDescription"),
-    errorCategory = qs("#errorCategory"),
-    errorForm = qs("#errorForm");
-  //errors;
+    errorName = qs('#errorName'), //****************** capturo los spans por id ************/
+    errorPrice = qs('#errorPrice'),
+    errorDiscount = qs('#errorDiscount'),
+    errorDescription = qs('#errorDescription'),
+    errorCategory = qs('#errorCategory'),
+    errorForm = qs('#errorForm');
+  /*   errors; */
 
   //validación por campo
 
-  name.addEventListener("blur", () => {
+  name.addEventListener('blur', () => {
     //uso event blur
     switch (
       true //uso switch para validar cada condición del campo
     ) {
       case !name.value: //si existe este error
-        name.classList.add("is-invalid"); // uso clase de bootstrap
-        errorName.innerHTML = "Enter name"; //mostrar leyenda del error
+        name.classList.add('is-invalid'); // uso clase de bootstrap
+        errorName.innerHTML = 'Enter name'; //mostrar leyenda del error
         errors = true;
         break;
       case name.value.length < 5:
-        name.classList.add("is-invalid"); // uso clase de bootstrap
-        errorName.innerHTML = "At least SIIII characters"; //mostrar leyenda del error
+        name.classList.add('is-invalid'); // uso clase de bootstrap
+        errorName.innerHTML = 'At least 5 characters'; //mostrar leyenda del error
         errors = true;
         break;
       default:
-        name.classList.remove("is-invalid");
-        name.classList.add("is-valid");
-        errorName.innerHTML = "";
+        name.classList.remove('is-invalid');
+        name.classList.add('is-valid');
+        errorName.innerHTML = '';
         errors = false;
         break;
     }
   }); //end --- name.addEvent
 
-  price.addEventListener("blur", () => {
+  price.addEventListener('blur', () => {
     switch (true) {
       case !price.value:
-        price.classList.add("is-invalid");
-        errorPrice.innerHTML = "Enter price";
+        price.classList.add('is-invalid');
+        errorPrice.innerHTML = 'Enter price';
         errors = true;
         break;
       case price.value < 0:
-        price.classList.add("is-invalid");
-        errorPrice.innerHTML = "Price must be more than 0";
+        price.classList.add('is-invalid');
+        errorPrice.innerHTML = 'Price must be more than 0';
         errors = true;
         break;
       default:
-        price.classList.remove("is-invalid");
-        price.classList.add("is-valid");
-        errorPrice.innerHTML = "";
+        price.classList.remove('is-invalid');
+        price.classList.add('is-valid');
+        errorPrice.innerHTML = '';
         errors = false;
         break;
     }
   }); //end --- price.addEvent
 
-  discount.addEventListener("blur", () => {
+  discount.addEventListener('blur', () => {
     switch (true) {
       case !discount.value:
-        discount.classList.add("is-invalid");
-        errorDiscount.innerHTML = "Enter discount";
+        discount.classList.add('is-invalid');
+        errorDiscount.innerHTML = 'Enter discount';
         errors = true;
         break;
       case discount.value < 0:
-        discount.classList.add("is-invalid");
-        errorDiscount.innerHTML = "discount must be more than 0";
+        discount.classList.add('is-invalid');
+        errorDiscount.innerHTML = 'discount must be more than 0';
         errors = true;
         break;
       default:
-        discount.classList.remove("is-invalid");
-        discount.classList.add("is-valid");
-        errorDiscount.innerHTML = "";
+        discount.classList.remove('is-invalid');
+        discount.classList.add('is-valid');
+        errorDiscount.innerHTML = '';
         errors = false;
         break;
     }
   }); //end --- discount.addEvent
 
-  description.addEventListener("blur", () => {
+  description.addEventListener('blur', () => {
     switch (true) {
       case !description.value:
-        description.classList.add("is-invalid");
-        errorDescription.innerHTML = "Enter description";
+        description.classList.add('is-invalid');
+        errorDescription.innerHTML = 'Enter description';
         errors = true;
         break;
       case description.value.length <= 20:
-        description.classList.add("is-invalid");
-        errorDescription.innerHTML =
-          "description must be more than 20 characters";
+        description.classList.add('is-invalid');
+        errorDescription.innerHTML = 'description must be more than 20 characters';
         errors = true;
         break;
       default:
-        description.classList.remove("is-invalid");
-        description.classList.add("is-valid");
-        errorDescription.innerHTML = "";
+        description.classList.remove('is-invalid');
+        description.classList.add('is-valid');
+        errorDescription.innerHTML = '';
         errors = false;
         break;
     }
   }); //end --- description.addEvent
 
-  category.addEventListener("blur", () => {
+  category.addEventListener('blur', () => {
     switch (true) {
       case !category.value: // == 'Categories'   --tampoco funciona, se generan dos clases
-        category.classList.add("is-invalid");
-        errorCategory.innerHTML = "Select a cetegory";
+        category.classList.add('is-invalid');
+        errorCategory.innerHTML = 'Select a cetegory';
         errors = true;
         break;
       default:
-        category.classList.remove("is-invalid");
-        category.classList.add("is-valid");
-        errorCategory.innerHTML = "";
+        category.classList.remove('is-invalid');
+        category.classList.add('is-valid');
+        errorCategory.innerHTML = '';
         errors = false;
         break;
     }
   }); //end --- category.addEvent
 
-  image.addEventListener("change", () => {
+  image.addEventListener('change', () => {
     switch (true) {
       case !regExExtensions.exec(image.value):
-        image.classList.add("is-invalid");
-        errorImage.innerHTML = "Only jpg, jpge, png, gif";
+        image.classList.add('is-invalid');
+        errorImage.innerHTML = 'Only jpg, jpge, png, gif';
         errors = true;
         break;
       default:
-        image.classList.remove("is-invalid");
-        image.classList.add("is-valid");
-        errorImage.innerHTML = "";
+        image.classList.remove('is-invalid');
+        image.classList.add('is-valid');
+        errorImage.innerHTML = '';
         errors = false;
         break;
     }
@@ -156,7 +156,7 @@ window.addEventListener("load", () => {
   //   }
   // });
 
-  addProduct.addEventListener("submit", (e) => {
+  addProduct.addEventListener('submit', (e) => {
     let errors = true;
     e.preventDefault(); //evita que se ejecute directamente el boton del form.
 
@@ -165,20 +165,17 @@ window.addEventListener("load", () => {
 
     for (let i = 0; i < elementsForm.length - 2; i++) {
       // -1 porque no tomo el boton submit
-      if (
-        elementsForm[i].value === "" ||
-        elementsForm[i].classList.contains("is-invalid")
-      ) {
-        elementsForm[i].classList.add("is-invalid");
-        errorForm.innerHTML = "Complete the required inputs";
+      if (elementsForm[i].value === '' || elementsForm[i].classList.contains('is-invalid')) {
+        elementsForm[i].classList.add('is-invalid');
+        errorForm.innerHTML = 'Complete the required inputs';
         errors = true;
       } else {
         errors = false;
       }
     }
     if (errors == false) {
-      errorForm.innerHTML = "";
-      alert("Product added");
+      errorForm.innerHTML = '';
+      alert('Product added');
       addProduct.submit();
     }
   });
--- CeroTempo-Site/src/validations/productValidation.js
+++ CeroTempo-Site/src/validations/productValidation.js
@@ -1,25 +1,25 @@
-const { check } = require("express-validator");
-const db = require("../database/models");
+const {check} = require('express-validator');
+const db = require('../database/models');
 
 module.exports = [
-  check("name")
+  check('name')
     .notEmpty()
     .withMessage("Enter product's name")
-    .isLength({ min: 5, max: 20 })
-    .withMessage("At least 5 characters")
+    .isLength({min: 5})
+    .withMessage('At least 5 characters')
     .bail(),
 
-  check("price").notEmpty().withMessage("Enter product's price").bail(),
+  check('price').notEmpty().withMessage("Enter product's price").bail(),
 
-  check("discount").notEmpty().withMessage("Enter product's discount").bail(),
+  check('discount').notEmpty().withMessage("Enter product's discount").bail(),
 
-  check("description")
+  check('description')
     .notEmpty()
     .withMessage("Enter product's description")
     .bail()
-    .isLength({ min: 20 })
-    .withMessage("At least 20 characters")
+    .isLength({min: 20})
+    .withMessage('At least 20 characters')
     .bail(),
 
-  check("category").notEmpty().withMessage("Enter a category").bail(),
+  check('category').notEmpty().withMessage('Enter a category').bail(),
 ];
