b27fb29c263383028de5353445aba9ae96bf1273
daniel-britos <danielbritospersonal@gmail.com>
2022-08-12 12:07:41 -0300

    category

--- CeroTempo-Site/src/controllers/productController.js
+++ CeroTempo-Site/src/controllers/productController.js
@@ -1,33 +1,33 @@
-const db = require('../database/models');
-const {Op} = require('sequelize');
+const db = require("../database/models");
+const { Op } = require("sequelize");
 module.exports = {
   pMain: (req, res) => {
     db.Product.findAll({
-      include: ['images'],
+      include: ["images"],
     })
       .then((products) => {
-        return res.render('productMain', {
+        return res.render("productMain", {
           products,
         });
       })
       .catch((error) => console.log(error));
   },
   pCart: (req, res) => {
-    res.render('productCart');
+    res.render("productCart");
   },
   pDetail: (req, res) => {
     db.Product.findByPk(req.params.id, {
-      include: ['images'],
+      include: ["images"],
     })
       .then((product) => {
-        return res.render('productDetail', {
+        return res.render("productDetail", {
           product,
         });
       })
       .catch((error) => console.log(error));
   },
   pSearch: (req, res) => {
-    const {keywords} = req.query;
+    const { keywords } = req.query;
     db.Product.findAll({
       where: {
         [Op.or]: [
@@ -43,12 +43,72 @@ module.exports = {
           },
         ],
       },
-      include: ['images'],
+      include: ["images"],
     })
       .then((products) => {
-        return res.render('search', {
+        return res.render("search", {
           products,
           keywords,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  pStrings: (req, res) => {
+    db.Product.findAll({
+      include: ["images"],
+    })
+      .then((products) => {
+        let categories = products.filter((product) => product.categoryId === 1);
+        res.render("categoryStrings", {
+          categories,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  pWind: (req, res) => {
+    db.Product.findAll({
+      include: ["images"],
+    })
+      .then((products) => {
+        let categories = products.filter((product) => product.categoryId === 2);
+        res.render("categoryWind", {
+          categories,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  pPercussion: (req, res) => {
+    db.Product.findAll({
+      include: ["images"],
+    })
+      .then((products) => {
+        let categories = products.filter((product) => product.categoryId === 3);
+        res.render("categoryPercussion", {
+          categories,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  pIdiophones: (req, res) => {
+    db.Product.findAll({
+      include: ["images"],
+    })
+      .then((products) => {
+        let categories = products.filter((product) => product.categoryId === 4);
+        res.render("categoryIdiophones", {
+          categories,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  pElectronics: (req, res) => {
+    db.Product.findAll({
+      include: ["images"],
+    })
+      .then((products) => {
+        let categories = products.filter((product) => product.categoryId === 5);
+        res.render("categoryElectronics", {
+          categories,
         });
       })
       .catch((error) => console.log(error));
--- CeroTempo-Site/src/routes/products.js
+++ CeroTempo-Site/src/routes/products.js
@@ -1,14 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const profileCheck = require('../middlewares/profileCheck');
+const profileCheck = require("../middlewares/profileCheck");
 
-const {pCart, pDetail, pMain, pSearch} = require('../controllers/productController');
+const {
+  pCart,
+  pDetail,
+  pMain,
+  pSearch,
+  pStrings,
+  pWind,
+  pPercussion,
+  pIdiophones,
+  pElectronics,
+} = require("../controllers/productController");
 
-
-router.get('/productMain', pMain);
-router.get('/productCart', profileCheck, pCart);
-router.get('/productDetail/:id', pDetail);
+router.get("/productMain", pMain);
+router.get("/productCart", profileCheck, pCart);
+router.get("/productDetail/:id", pDetail);
 router.get("/search", pSearch);
-
+router.get("/categoryStrings", pStrings);
+router.get("/categoryWind", pWind);
+router.get("/categoryPercussion", pPercussion);
+router.get("/categoryIdiophones", pIdiophones);
+router.get("/categoryElectronics", pElectronics);
 
 module.exports = router;
